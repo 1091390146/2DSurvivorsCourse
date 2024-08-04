267012e908000f474a25c1c6b76896d3a6ff718a
pan <1091390146@qq.com>
2024-08-04 21:34:18 +0800

    19 player health bar

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -3,6 +3,8 @@ extends Node
 class_name HealthComponent
 
 signal died
+signal health_changed
+signal health_decreased
 
 @export var max_health: float = 10
 
@@ -12,9 +14,16 @@ var current_health
 func _ready():
 	current_health = max_health # Replace with function body.
 
+func get_health_percent():
+	if max_health <= 0:
+		return 0
+	return min(current_health / max_health, 1)
 
 func damage(damage_amount:float):
 	current_health = max(current_health - damage_amount, 0)
+	health_changed.emit()
+	if damage_amount > 0:
+		health_decreased.emit()
 	Callable(check_death).call_deferred()
 	
 func check_death():
